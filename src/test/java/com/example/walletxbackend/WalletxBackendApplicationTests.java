269ba0bfa5dcdbd170c05c61a7d318e3b3d92953
NarendraValluru <narendravalluru03@gmail.com>
2026-08-19 02:42:16 +0530

    chore: initialize WalletX backend

---
+++ src/test/java/com/example/walletxbackend/WalletxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
